e4e4cac5df687e642aea8f79dceb231c208d389f
Huseyn Qurbanov <72246234+HuseynagaQurbanov@users.noreply.github.com>
2024-03-29 22:52:32 +0400

    Seperated panel structure added

---
+++ ResumeDesktopApp/src/main/java/com/resume/config/Config.java
@@ -0,0 +1,7 @@
+package com.resume.config;
+
+import com.entity.User;
+
+public class Config {
+    public static User loggedUser;
+}
---
+++ ResumeDesktopApp/src/main/java/com/resume/main/UserMain.java
@@ -0,0 +1,201 @@
+package com.resume.main;
+
+import com.resume.config.Config;
+import com.dao.inter.UserDaoInter;
+import com.entity.User;
+import com.main.Context;
+
+public class UserMain extends javax.swing.JFrame {
+
+    private UserDaoInter userDao = Context.instanceUserDao();
+
+    public UserMain() {
+        initComponents();
+        Config.loggedUser = userDao.getById(1);
+        fillUserFullname();
+        profilePanel.fillUserComponents();
+        detailsPanel.fillUserComponents();
+    }
+    
+    private void fillUserFullname(){
+        txtName.setText(Config.loggedUser.getName());
+        txtSurname.setText(Config.loggedUser.getSurname());
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        separator = new javax.swing.JSeparator();
+        userInfo = new javax.swing.JPanel();
+        txtName = new javax.swing.JTextField();
+        lblName = new javax.swing.JLabel();
+        lblSurname = new javax.swing.JLabel();
+        txtSurname = new javax.swing.JTextField();
+        btnSave = new javax.swing.JButton();
+        tpUserInfo = new javax.swing.JTabbedPane();
+        profilePanel = new com.resume.panel.ProfilePanel();
+        detailsPanel = new com.resume.panel.DetailsPanel();
+        skillsPanel = new com.resume.panel.SkillsPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        txtName.setToolTipText("");
+        txtName.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtNameActionPerformed(evt);
+            }
+        });
+
+        lblName.setText("Name");
+
+        lblSurname.setText("Surname");
+
+        txtSurname.setToolTipText("");
+        txtSurname.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txtSurnameActionPerformed(evt);
+            }
+        });
+
+        btnSave.setText("Save");
+        btnSave.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnSaveActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout userInfoLayout = new javax.swing.GroupLayout(userInfo);
+        userInfo.setLayout(userInfoLayout);
+        userInfoLayout.setHorizontalGroup(
+            userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(userInfoLayout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(lblSurname)
+                    .addComponent(lblName))
+                .addGap(69, 69, 69)
+                .addGroup(userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtName, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(txtSurname, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.PREFERRED_SIZE, 100, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(btnSave)
+                .addGap(16, 16, 16))
+        );
+        userInfoLayout.setVerticalGroup(
+            userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(userInfoLayout.createSequentialGroup()
+                .addGap(17, 17, 17)
+                .addGroup(userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblName)
+                    .addComponent(txtName, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(userInfoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtSurname, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblSurname)
+                    .addComponent(btnSave))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+
+        tpUserInfo.addTab("Profile", profilePanel);
+        tpUserInfo.addTab("Details", detailsPanel);
+        tpUserInfo.addTab("Skills", skillsPanel);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(separator)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(19, 19, 19)
+                .addComponent(userInfo, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addContainerGap())
+            .addComponent(tpUserInfo)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(userInfo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(separator, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(tpUserInfo))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void txtNameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtNameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtNameActionPerformed
+
+    private void txtSurnameActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtSurnameActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txtSurnameActionPerformed
+
+    private void btnSaveActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSaveActionPerformed
+        User user = Config.loggedUser;
+
+        String name = txtName.getText();
+        String surname = txtSurname.getText();
+
+        user.setName(name);
+        user.setSurname(surname);
+
+        profilePanel.fillUser(user);
+        detailsPanel.fillUser(user);
+
+        userDao.updateUser(user);
+    }//GEN-LAST:event_btnSaveActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(UserMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(UserMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(UserMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(UserMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new UserMain().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnSave;
+    private com.resume.panel.DetailsPanel detailsPanel;
+    private javax.swing.JLabel lblName;
+    private javax.swing.JLabel lblSurname;
+    private com.resume.panel.ProfilePanel profilePanel;
+    private javax.swing.JSeparator separator;
+    private com.resume.panel.SkillsPanel skillsPanel;
+    private javax.swing.JTabbedPane tpUserInfo;
+    private javax.swing.JTextField txtName;
+    private javax.swing.JTextField txtSurname;
+    private javax.swing.JPanel userInfo;
+    // End of variables declaration//GEN-END:variables
+}
